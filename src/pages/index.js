9ec944f7bb26de8b25b928a0fad7af489695b7cb
Cole Essig <hoopwalker@yahoo.com>
2024-09-13 19:37:30 -0700

    feat/added functions to find closest span to edge of reel div

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,8 @@ const thirdSlot = document.querySelector(".slots__third");
 const slotsButton = document.querySelector(".slots__button");
 const slotsButtonPopup = document.querySelector(".slots__button_popup");
 
-function getRandomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// FUNCTIONS
+
 
 function runReel() {
     firstSlot.classList.add(`slots__first_animation`);
@@ -38,6 +37,55 @@ function highlightResultsButton() {
   slotsButtonPopup.classList.add('slots__button_popup-visible')
 }
 
+function findClosestReelSpan(div) {
+  const spans = div.querySelectorAll("span")
+  
+  let resultArray = [];
+
+  spans.forEach(span => {
+    const spanRect = span.getBoundingClientRect();
+    const divRect = div.getBoundingClientRect();
+
+    const distanceToTop = divRect.top - spanRect.top;
+    const distanceToBottom = divRect.bottom - spanRect.bottom;
+
+    const distance = Math.min(distanceToTop, distanceToBottom);
+    resultArray.push(distance);
+  });
+  closestToZero(resultArray);
+  console.log(closestToZero(resultArray));
+}
+
+function closestToZero(numbers) {
+  let closest = 0;
+  let index;
+    
+    for (let i = 0; i < numbers.length ; i++) {
+        if (closest === 0) {
+            closest = numbers[i];
+            index = [i];
+        } else if (numbers[i] > 0 && numbers[i] <= Math.abs(closest)) {
+            closest = numbers[i];
+            index = [i];
+        } else if (numbers[i] < 0 && - numbers[i] < Math.abs(closest)) {
+            closest = numbers[i];
+            index = [i];
+        }
+    }
+    
+    return [closest, index];
+}
+
+function centerReelAnimation(div) {
+  
+}
+
+function getRandomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// HANDLERS
+
 function handleDaddyButtonClick() {
   runReel();
   setTimeout(() => {
@@ -46,10 +94,14 @@ function handleDaddyButtonClick() {
   }, getRandomNumberBetween(2000, 6000))
 }
 
+// EVENTLISTENERS
+
 slotsButton.addEventListener("click", () => {
   handleDaddyButtonClick();
 })
 
 slotsButtonPopup.addEventListener("click", () => {
-  resetReel();
+  // resetReel();
+  findClosestReelSpan(firstSlot);
 })
+
