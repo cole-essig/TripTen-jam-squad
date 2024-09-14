ac978b74ad04d6acd2425b613d2f5244008ddfba
Cole Essig <hoopwalker@yahoo.com>
2024-09-14 08:09:21 -0700

    Made slot wheel functional, refactored with gif image vs finding nearest span position

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,18 @@ const slotsButtonPopup = document.querySelector(".slots__button_popup");
 
 // FUNCTIONS
 
-
 function runReel() {
     firstSlot.classList.add(`slots__first_animation`);
     secondSlot.classList.add(`slots__second_animation`);
     thirdSlot.classList.add(`slots__third_animation`);
 };
 
-function pauseReel() {
-    firstSlot.classList.add(`slots__paused`);
-    secondSlot.classList.add(`slots__paused`);
-    thirdSlot.classList.add(`slots__paused`)
+function pauseReel(div) {
+  div.classList.add(`slots__paused`);
 }
 
 function resetReel() {
-  firstSlot.classList.remove(`slots__first_animation`);
+    firstSlot.classList.remove(`slots__first_animation`);
     secondSlot.classList.remove(`slots__second_animation`);
     thirdSlot.classList.remove(`slots__third_animation`);
 
@@ -31,53 +28,39 @@ function resetReel() {
     thirdSlot.classList.remove(`slots__paused`);
 
     slotsButtonPopup.classList.remove('slots__button_popup-visible');
+
+    resetSpan(firstSlot);
+    resetSpan(secondSlot);
+    resetSpan(thirdSlot);
+}
+
+function resetSpan(div) {
+  div.classList.remove("slots__finished");
+  const childSpans = div.querySelectorAll('*');
+  
+  childSpans.forEach(span => {
+    span.classList.remove("slots__hidden_numbers")
+  })
 }
 
 function highlightResultsButton() {
   slotsButtonPopup.classList.add('slots__button_popup-visible')
 }
 
-function findClosestReelSpan(div) {
-  const spans = div.querySelectorAll("span")
+function finishReelAnimation(div) {
+  pauseReel(div);
+  div.classList.add("slots__finished");
+  const childSpans = div.querySelectorAll('*');
   
-  let resultArray = [];
-
-  spans.forEach(span => {
-    const spanRect = span.getBoundingClientRect();
-    const divRect = div.getBoundingClientRect();
-
-    const distanceToTop = divRect.top - spanRect.top;
-    const distanceToBottom = divRect.bottom - spanRect.bottom;
-
-    const distance = Math.min(distanceToTop, distanceToBottom);
-    resultArray.push(distance);
-  });
-  closestToZero(resultArray);
-  console.log(closestToZero(resultArray));
+  childSpans.forEach(span => {
+    span.classList.add("slots__hidden_numbers")
+  })
 }
 
-function closestToZero(numbers) {
-  let closest = 0;
-  let index;
-    
-    for (let i = 0; i < numbers.length ; i++) {
-        if (closest === 0) {
-            closest = numbers[i];
-            index = [i];
-        } else if (numbers[i] > 0 && numbers[i] <= Math.abs(closest)) {
-            closest = numbers[i];
-            index = [i];
-        } else if (numbers[i] < 0 && - numbers[i] < Math.abs(closest)) {
-            closest = numbers[i];
-            index = [i];
-        }
-    }
-    
-    return [closest, index];
-}
-
-function centerReelAnimation(div) {
-  
+function getCoinReward(first, second, third) {
+  finishReelAnimation(first);
+  finishReelAnimation(second);
+  finishReelAnimation(third);
 }
 
 function getRandomNumberBetween(min, max) {
@@ -89,9 +72,9 @@ function getRandomNumberBetween(min, max) {
 function handleDaddyButtonClick() {
   runReel();
   setTimeout(() => {
-    pauseReel();
     highlightResultsButton();
-  }, getRandomNumberBetween(2000, 6000))
+    getCoinReward(firstSlot, secondSlot, thirdSlot);
+  }, getRandomNumberBetween(3000, 7000))
 }
 
 // EVENTLISTENERS
@@ -101,7 +84,6 @@ slotsButton.addEventListener("click", () => {
 })
 
 slotsButtonPopup.addEventListener("click", () => {
-  // resetReel();
-  findClosestReelSpan(firstSlot);
+  resetReel();
 })
 
