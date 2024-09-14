68cb65e17b4dff7daffaaca0a9c9f503a99eac6c
Cole Essig <hoopwalker@yahoo.com>
2024-09-13 17:32:58 -0700

    Added reset button and funtionality to slot reel, added css classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,11 @@ const secondSlot = document.querySelector(".slots__second");
 const thirdSlot = document.querySelector(".slots__third");
 
 const slotsButton = document.querySelector(".slots__button");
+const slotsButtonPopup = document.querySelector(".slots__button_popup");
+
+function getRandomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function runReel() {
     firstSlot.classList.add(`slots__first_animation`);
@@ -11,19 +16,40 @@ function runReel() {
     thirdSlot.classList.add(`slots__third_animation`);
 };
 
-function stopReel() {
+function pauseReel() {
     firstSlot.classList.add(`slots__paused`);
     secondSlot.classList.add(`slots__paused`);
     thirdSlot.classList.add(`slots__paused`)
 }
 
-function handleButtonClick() {
+function resetReel() {
+  firstSlot.classList.remove(`slots__first_animation`);
+    secondSlot.classList.remove(`slots__second_animation`);
+    thirdSlot.classList.remove(`slots__third_animation`);
+
+    firstSlot.classList.remove(`slots__paused`);
+    secondSlot.classList.remove(`slots__paused`);
+    thirdSlot.classList.remove(`slots__paused`);
+
+    slotsButtonPopup.classList.remove('slots__button_popup-visible');
+}
+
+function highlightResultsButton() {
+  slotsButtonPopup.classList.add('slots__button_popup-visible')
+}
+
+function handleDaddyButtonClick() {
   runReel();
   setTimeout(() => {
-    stopReel();
-  }, 4000)
+    pauseReel();
+    highlightResultsButton();
+  }, getRandomNumberBetween(2000, 6000))
 }
 
 slotsButton.addEventListener("click", () => {
-  handleButtonClick();
+  handleDaddyButtonClick();
+})
+
+slotsButtonPopup.addEventListener("click", () => {
+  resetReel();
 })
